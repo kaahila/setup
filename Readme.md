5bd6020d56a26390b7f1b879f710ae0c2a64513d
kaahila <dr.doomxp@gmail.com>
2025-06-26 14:10:44 +0200

    update

--- Readme.md
+++ Readme.md
@@ -1,10 +1,17 @@
 # My personal setup script for Windows
 
-This is a script that I use to install all the programs I need to work on my Windows machine.
+This is a script that I use to install all the programs I need to work on my Windows machine. It will automatically request administrator privileges to install the software.
 
+### Quick Start
 
-### Quick start
+This command will download and run the script, installing the default set of development tools.
+
 ```powershell
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/kaahila/setup/refs/heads/master/setup.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('[https://raw.githubusercontent.com/kaahila/setup/refs/heads/master/setup.ps1](https://raw.githubusercontent.com/kaahila/setup/refs/heads/master/setup.ps1)'))
 ```
 
+### Quick Start with game launchers
+This single command will download and run the script with the game installation option enabled
+```powershell
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex "& { $( (New-Object Net.WebClient).DownloadString('[https://raw.githubusercontent.com/kaahila/setup/refs/heads/master/setup.ps1](https://raw.githubusercontent.com/kaahila/setup/refs/heads/master/setup.ps1)') ) } -installGames"
+```
--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,17 @@
-#Elevate to admin
+# To run this script and install the game clients, execute it with the -installGames switch:
+# .\your_script_name.ps1 -installGames
+#
+# To run without installing games, simply execute the script:
+# .\your_script_name.ps1
+
+[CmdletBinding()]
+param (
+    [Switch]$installGames
+)
+
 # Get the ID and security principal of the current user account
 $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal = new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID)
 
 # Get the security principal for the Administrator role
 $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
@@ -9,118 +19,146 @@ $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 # Check to see if we are currently running "as Administrator"
 if ($myWindowsPrincipal.IsInRole($adminRole)) {
     # We are running "as Administrator" - so change the title and background color to indicate this
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Elevated)"
     $Host.UI.RawUI.BackgroundColor = "DarkBlue"
-    clear-host
-} else {
-    #Load the Background image
+    Clear-Host
+}
+else {
+    # We are not running "as Administrator" - so relaunch as administrator
+    
+    # --- Set Desktop Background ---
+    # Load the Background image
     # Define the URL and the destination path
     $url = "https://raw.githubusercontent.com/kaahila/setup/refs/heads/master/background.jpg"
-    $wallpaperPath = "$env:USERPROFILE\Pictures\background.jpg"
+    $picturesPath = [Environment]::GetFolderPath('MyPictures')
+    $wallpaperPath = Join-Path -Path $picturesPath -ChildPath "background.jpg"
+
+    # Create the Pictures directory if it doesn't exist
+    if (-not (Test-Path -Path $picturesPath)) {
+        New-Item -ItemType Directory -Path $picturesPath | Out-Null
+    }
 
     # Download the file
-    Invoke-WebRequest -Uri $url -OutFile $wallpaperPath
+    try {
+        Invoke-WebRequest -Uri $url -OutFile $wallpaperPath -ErrorAction Stop
+        Write-Output "Background Image downloaded to $wallpaperPath"
 
-    # Confirm download
-    Write-Output "Background Image downloaded to $wallpaperPath"
-
-    #Does $Profile exist?
-    if (-not (Test-Path $Profile)) {
-        New-Item -Path $PROFILE -Type File -Force
+        # Set Desktop background using C# code within PowerShell
+        Add-Type -TypeDefinition @"
+        using System.Runtime.InteropServices;
+        public class Wallpaper {
+            public const uint SPI_SETDESKWALLPAPER = 0x0014;
+            public const uint SPIF_UPDATEINIFILE = 0x01;
+            public const uint SPIF_SENDWININICHANGE = 0x02;
+            [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            private static extern int SystemParametersInfo (uint uAction, uint uParam, string lpvParam, uint fuWinIni);
+            public static void SetWallpaper (string path) {
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            }
+        }
+"@
+        [Wallpaper]::SetWallpaper($wallpaperPath)
+        Write-Output "Desktop wallpaper has been set."
     }
-
-    #Write to the profile scrip
-    if (Select-String -Path $PROFILE -Pattern "oh-my-posh init pwsh" -Quiet) {
-        Write-Output "oh-my-posh is already initialized in the profile script."
-    } else {
-        Add-Content $PROFILE ( -join ("oh-my-posh init pwsh --config '$env:POSH_THEMES_PATH\if_tea.omp.json' | Invoke-Expression"))
-        Write-Output "Initialized oh-my-posh in the profile script."
+    catch {
+        Write-Warning "Failed to download or set wallpaper. Error: $($_.Exception.Message)"
     }
     
-    #Set Desktop background
-    Add-Type -TypeDefinition @'
-    using System.Runtime.InteropServices;
-    public class Wallpaper {
-        public const uint SPI_SETDESKWALLPAPER = 0x0014;
-        public const uint SPIF_UPDATEINIFILE = 0x01;
-        public const uint SPIF_SENDWININICHANGE = 0x02;
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern int SystemParametersInfo (uint uAction, uint uParam, string lpvParam, uint fuWinIni);
-        public static void SetWallpaper (string path) {
-            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
-        }
-    }
-'@
-    [Wallpaper]::SetWallpaper($wallpaperPath)
-
-    # We are not running "as Administrator" - so relaunch as administrator
+    # --- Relaunch with Elevation ---
     # Create a new process object that starts PowerShell
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
 
-    # Specify the current script path and name as a parameter
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    # Pass the current script path and any parameters to the new process
+    $arguments = "-File `"$($myInvocation.MyCommand.Definition)`""
+    if ($installGames) {
+        $arguments += " -installGames"
+    }
+    $newProcess.Arguments = $arguments
 
     # Indicate that the process should be elevated
-    $newProcess.Verb = "runas";
+    $newProcess.Verb = "runas"
 
     # Start the new process
-    [System.Diagnostics.Process]::Start($newProcess);
+    try {
+        [System.Diagnostics.Process]::Start($newProcess)
+    }
+    catch {
+        Write-Error "Failed to relaunch script with administrator privileges. Please run the script as an Administrator manually."
+    }
+    
 
     # Exit from the current, unelevated, process
     exit
 }
 
-Write-Output "Installing Programms."
-#Winget to install dbeaver
-winget install dbeaver.dbeaver -s winget
+# --- Software Installation ---
+Write-Output "Starting software installation."
 
-#Winget to install oh-my-posh
-winget install JanDeDobbeleer.OhMyPosh -s winget
+# Winget to install dbeaver
+winget install dbeaver.dbeaver -s winget --accept-package-agreements --accept-source-agreements
 
-#Winget zen browser
-winget install Zen-Team.Zen-Browser.Optimized -s winget --location "C:\Program Files\Zen Browser"
+# Winget to install Brave
+winget install Brave.Brave -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install 7zip
-winget install 7zip.7zip -s winget
+# winget to install 7zip
+winget install 7zip.7zip -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install putty
-winget install putty.putty -s winget
+# winget to install putty
+winget install putty.putty -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install notepad++
-winget install notepadplusplus.notepadplusplus -s winget
+# winget to install notepad++
+winget install notepadplusplus.notepadplusplus -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install vscode
-winget install microsoft.vscode -s winget
+# winget to install vscode
+winget install microsoft.vscode -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install python 3.13.0
-winget install Python.Python.3.13 -s winget
+# winget to install python 3.13.0
+winget install Python.Python.3 -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install vlc
-winget install VideoLAN.VLC -s winget
+# winget to install vlc
+winget install VideoLAN.VLC -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install winscp
-winget install WinSCP.WinSCP -s winget
+# winget to install winscp
+winget install WinSCP.WinSCP -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install windirstat
-winget install WinDirStat.WinDirStat -s winget
+# winget to install windirstat
+winget install WinDirStat.WinDirStat -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install winrar
-winget install RARLab.WinRAR -s winget
+# winget to install winrar
+winget install RARLab.WinRAR -s winget --accept-package-agreements --accept-source-agreements
 
-#install git
-winget install Git.Git -s winget
+# install git
+winget install Git.Git -s winget --accept-package-agreements --accept-source-agreements
 
-#winget to install java 17 and 21
-winget install Microsoft.OpenJDK.21 -s winget
-winget install Microsoft.OpenJDK.17 -s winget
+# winget to install java 17 and 21
+winget install Microsoft.OpenJDK.21 -s winget --accept-package-agreements --accept-source-agreements
+winget install Microsoft.OpenJDK.17 -s winget --accept-package-agreements --accept-source-agreements
 
-# Reload environment variables
+
+# --- Optional Game Client Installation ---
+if ($installGames) {
+    Write-Output "Installing Game Clients as requested."
+    
+    # Install Steam
+    winget install Valve.Steam -s winget --accept-package-agreements --accept-source-agreements
+
+    # Install Riot Client
+    winget install RiotGames.RiotClient -s winget --accept-package-agreements --accept-source-agreements
+    
+    # Install Ubisoft Connect (Uplay)
+    winget install Ubisoft.Connect -s winget --accept-package-agreements --accept-source-agreements
+
+    # Install Playnite
+    winget install Playnite.Playnite -s winget --accept-package-agreements --accept-source-agreements
+} else {
+    Write-Output "Skipping game client installation. To install them, run the script with the '-installGames' switch."
+}
+
+
+# --- Finalization ---
+Write-Output "Reloading environment variables to apply changes."
+# Reload environment variables to make sure new paths (like for Git, Java, Python) are available
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) + ";" +
             [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
 
-Write-Output "Environment variables reloaded."
-
-oh-my-posh font install JetBrainsMono
-
-[System.Diagnostics.Process]::Start("zen", "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/")
-[System.Diagnostics.Process]::Start("Code")
+Write-Output "Script finished. All selected programs are installed."
